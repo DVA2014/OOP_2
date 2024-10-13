78d21f955ee088c26d7610e36e8575ec44ef3312
Vyacheslav Dernovoy <2014dva@gmail.com>
2024-10-13 21:19:44 +0500

    exercise_2

--- main.py
+++ main.py
@@ -7,7 +7,23 @@ with open('recipes.txt', encoding='utf-8') as file:
         for _ in range(num_ingredients):
             ingredient_info = file.readline().strip().split(' | ')
             ingredients.append({'ingredient_name': ingredient_info[0],
-            'quantity': ingredient_info[1], 'measure': ingredient_info[2]})
+            'quantity': int(ingredient_info[1]), 'measure': ingredient_info[2]})
         cook_book[dish] = ingredients
         file.readline()
     print(cook_book)
+
+def get_shop_list_by_dishes(dishes: list, person_count: int):
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                if ingredient['ingredient_name'] in shop_list:
+                    shop_list[ingredient['ingredient_name']]['quantity'] += (
+                        ingredient['quantity'] * person_count)
+                else:
+                    shop_list[ingredient['ingredient_name']] = (
+{'measure': ingredient['measure'],'quantity': (ingredient['quantity'] * person_count)})
+        else:
+            print('Такого блюда нет в книге')
+    print(shop_list)
+get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
